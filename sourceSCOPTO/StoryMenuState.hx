13e0381977cb4629f7b49c80637e4fbd923376f1
shpeelock <xxlromas@gmail.com>
2021-07-15 18:09:15 +0300

    white story bg lol
    Yeah

--- sourceSCOPTO/StoryMenuState.hx
+++ sourceSCOPTO/StoryMenuState.hx
@@ -86,7 +86,7 @@ class StoryMenuState extends MusicBeatState
 		rankText.screenCenter(X);
 
 		var ui_tex = Paths.getSparrowAtlas('campaign_menu_UI_assets');
-		var yellowBG:FlxSprite = new FlxSprite(0, 56).makeGraphic(FlxG.width, 400, 0xFFF9CF51);
+		var yellowBG:FlxSprite = new FlxSprite(0, 56).makeGraphic(FlxG.width, 400, FlxColor.WHITE);
 
 		grpWeekText = new FlxTypedGroup<MenuItem>();
 		add(grpWeekText);
