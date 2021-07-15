168554282d845b21e97eb0534f1a7ba2bad069e0
shpeelock <xxlromas@gmail.com>
2021-07-16 00:25:27 +0300

    fixed the font because im stupid
    AGGGGGGGGGHHHH I JATE THIS STUPID FONE

--- sourceSCOPTO/DialogueBox.hx
+++ sourceSCOPTO/DialogueBox.hx
@@ -308,12 +308,12 @@ class DialogueBox extends FlxSpriteGroup
 		}
 
 		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
-		dropText.font = 'vcr';
+		dropText.font = 'VCR OSD Mono';
 		dropText.color = 0xFFEDEDED;
 		add(dropText);
 
 		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
-		swagDialogue.font = 'vcr';
+		swagDialogue.font = 'VCR OSD Mono';
 		swagDialogue.color = 0xFFFFFFFF;
 		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
 		add(swagDialogue);
