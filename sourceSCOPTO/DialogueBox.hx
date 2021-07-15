afbffce49b16dffcf5a9594f696f393d5bccbe94
shpeelock <xxlromas@gmail.com>
2021-07-15 19:42:24 +0300

    replaced dialogue font to vcr
    yeah

--- sourceSCOPTO/DialogueBox.hx
+++ sourceSCOPTO/DialogueBox.hx
@@ -308,12 +308,12 @@ class DialogueBox extends FlxSpriteGroup
 		}
 
 		dropText = new FlxText(242, 502, Std.int(FlxG.width * 0.6), "", 32);
-		dropText.font = 'Comic Book';
+		dropText.font = 'vcr';
 		dropText.color = 0xFFEDEDED;
 		add(dropText);
 
 		swagDialogue = new FlxTypeText(240, 500, Std.int(FlxG.width * 0.6), "", 32);
-		swagDialogue.font = 'Comic Book';
+		swagDialogue.font = 'vcr';
 		swagDialogue.color = 0xFFFFFFFF;
 		swagDialogue.sounds = [FlxG.sound.load(Paths.sound('pixelText'), 0.6)];
 		add(swagDialogue);
