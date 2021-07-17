88cf06bc88fb3adcb6154bdcaf270e397a856e78
wildythomas <84243304+wildythomas@users.noreply.github.com>
2021-07-17 22:14:48 +0700

    Revert "some code stuff"
    This reverts commit 6a6d45a35ee056a8f27f4a1c16724168b9409407.

--- source/Note.hx
+++ source/Note.hx
@@ -18,7 +18,6 @@ class Note extends FlxSprite
 	public var strumTime:Float = 0;
 
 	public var mustPress:Bool = false;
-	public var warning:Bool = false;
 	public var noteData:Int = 0;
 	public var canBeHit:Bool = false;
 	public var tooLate:Bool = false;
@@ -56,13 +55,6 @@ class Note extends FlxSprite
 		if (this.strumTime < 0 )
 			this.strumTime = 0;
 
-		warning = noteData > 7;
-
-		if(isSustainNote && prevNote.warning) 
-		{ 
-			warning = true;
-		}
-		
 		this.noteData = noteData;
 
 		var daStage:String = PlayState.curStage;
@@ -115,31 +107,22 @@ class Note extends FlxSprite
 				setGraphicSize(Std.int(width * 0.7));
 				updateHitbox();
 				antialiasing = true;
-				if(warning)
-				{
-						loadGraphic(Paths.image('noteShit'));
-						x -= 50;
-				}
-
 		}
 
-		if(!warning)
+		switch (noteData)
 		{
-			switch (noteData)
-			{
-				case 0:
-					x += swagWidth * 0;
-					animation.play('purpleScroll');
-				case 1:
-					x += swagWidth * 1;
-					animation.play('blueScroll');
-				case 2:
-					x += swagWidth * 2;
-					animation.play('greenScroll');
-				case 3:
-					x += swagWidth * 3;
-					animation.play('redScroll');
-			}
+			case 0:
+				x += swagWidth * 0;
+				animation.play('purpleScroll');
+			case 1:
+				x += swagWidth * 1;
+				animation.play('blueScroll');
+			case 2:
+				x += swagWidth * 2;
+				animation.play('greenScroll');
+			case 3:
+				x += swagWidth * 3;
+				animation.play('redScroll');
 		}
 
 		// trace(prevNote);
